9676977c7434f4a365d34383b01330fbd2e016f9
aaahhhrav15 <lodhaaarav12005@gmail.com>
2025-06-02 12:14:03 +0530

    added gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  root: "./client",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-    },
-  },
-});
+export default defineConfig(({ mode }) => ({
+    root: "./client",
+    plugins: [
+        react(),
+        mode === 'development' && componentTagger(),
+      ].filter(Boolean),
+      server: {
+        host: "::",
+        port: 8080
+      },
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./client/src"),
+        },
+      },
+}));
