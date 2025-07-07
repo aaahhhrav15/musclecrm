f014a41e2bc96785f622f72778f4ba96b3caa297
aaahhhrav15 <lodhaaarav12005@gmail.com>
2025-07-07 14:33:22 +0530

    added the url for cors

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://crm-nine-indol.vercel.app'], // Your frontend URL
+  origin: ['http://localhost:5173', 'https://flexcrm-ui-suite.vercel.app'], // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
