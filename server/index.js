b4fd157a28135933c07eb08d459874532030918c
aaahhhrav15 <lodhaaarav12005@gmail.com>
2025-07-10 17:55:43 +0530

    added an origin for cors

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://MuscleCRM-ui-suite.vercel.app', 'https://www.musclecrm.com','https://musclecrm.com'], // Your frontend URL
+  origin: ['http://localhost:5173', 'https://MuscleCRM-ui-suite.vercel.app', 'https://www.musclecrm.com','https://musclecrm.com','http://musclecrm-frontend.s3-website.ap-south-1.amazonaws.com'], // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
